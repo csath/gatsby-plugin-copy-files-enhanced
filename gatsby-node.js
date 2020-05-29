add513adc011df8fff7583cc09f3756dea37c407
ChanakaA <chanakaathurugiriya@gmail.com>
2020-05-29 19:56:08 +0530

    enable regex

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,64 @@
 const path = require('path');
 const fsExtra = require('fs-extra');
 
+const getDirectories = source =>
+	fsExtra.readdirSync(source, { withFileTypes: true })
+		.filter(dirent => dirent.isDirectory())
+		.map(dirent => dirent.name)
+
+const regex1 = RegExp('(.*)\\/\\*(.*)');
+
 exports.onCreateNode = ({ node }, pluginOptions) => {
-	const { source, destination } = pluginOptions;
+	const { source, destination = '', purge = false } = pluginOptions;
 	const sourceNormalized = path.normalize(source);
 	if (node.internal.type === 'File') {
 		const dir = path.normalize(node.dir);
 		if (dir.includes(sourceNormalized)) {
 			const relativeToDest = dir.replace(sourceNormalized, '');
-			const newPath = path.join(
-				process.cwd(),
-				'public',
-				destination,
-				relativeToDest,
-				node.base
-			);
 
-			fsExtra.copy(node.absolutePath, newPath, err => {
-				if (err) {
-					console.error('Error copying file', err);
-				}
-			});
+			// if regex enabled
+			if (regex1.test(destination)) {
+				const hits = regex1.exec(destination)
+
+				if (!hits) return
+				const regPrefix = hits[1]
+				const regPostfix = hits[2]
+
+				const dirList = getDirectories(regPrefix)
+
+				dirList.forEach(e => {
+					const newDestination = regPrefix + '/' + e + regPostfix;
+					const newPath = path.join(
+						process.cwd(),
+						'public',
+						newDestination,
+						relativeToDest,
+						node.base
+					);
+
+					fsExtra.copy(node.absolutePath, newPath, { overwrite: purge }, err => {
+						if (err) {
+							console.error('Error copying file', err);
+						}
+					});
+				})
+			}
+			// if regex not enabled
+			else {
+				const newPath = path.join(
+					process.cwd(),
+					'public',
+					destination,
+					relativeToDest,
+					node.base
+				);
+
+				fsExtra.copy(node.absolutePath, newPath, { overwrite: purge }, err => {
+					if (err) {
+						console.error('Error copying file', err);
+					}
+				});
+			}
 		}
 	}
 };
